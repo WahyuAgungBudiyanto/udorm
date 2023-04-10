60536a73f73a0ed1dbab80912547246e0a2f93ca
Wahyu Agung <73398631+WahyuAgungBudiyanto@users.noreply.github.com>
2023-04-10 17:36:35 +0800

    fixing splash screen

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,6 +3,7 @@ import Logo from './logo.svg';
 import Face from './face.png';
 import ZoomOut from './zoomout.png';
 import ZoomIn from './zoomin.png';
-import BackHome from './backHome.png'
+import BackHome from './backHome.png';
+import mainBackground from './mainBackground.png'
 
-export {Exit, Logo, Face, ZoomOut, ZoomIn, BackHome};
+export {Exit, Logo, Face, ZoomOut, ZoomIn, BackHome, mainBackground};
--- src/pages/SplashScreen/index.js
+++ src/pages/SplashScreen/index.js
@@ -1,37 +1,53 @@
-import {StyleSheet, Text, View, Image, Dimensions} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  ImageBackground,
+} from 'react-native';
 import {useEffect} from 'react';
 import {Label} from '../../components';
-import {Logo} from '../../assets/images';
-import { getData } from '../../utils/LocalStorage';
+import {Logo, mainBackground} from '../../assets/images';
+import {getData} from '../../utils/LocalStorage';
+
 const {width, height} = Dimensions.get('window');
 
-const SplashScreen = (props) => {
+const SplashScreen = props => {
   useEffect(() => {
-   
     getData('tokenpn').then(data => {
-        getData('userSession').then(dataSession => {
-          if(dataSession){
-            setTimeout(() => {
-              props.navigation.replace( dataSession.userType == 'Student' ? 'HomeStudent' : 'HomeMonitor')
-            }, 1000)
-          }
-          else if (dataSession == null){
-            setTimeout(() => {
-              props.navigation.replace('SignIn', {tokenpn: data});
-            }, 1000);
-          }
-        });
+      getData('userSession').then(dataSession => {
+        if (dataSession) {
+          setTimeout(() => {
+            props.navigation.replace(
+              dataSession.userType == 'Student' ? 'HomeStudent' : 'HomeMonitor',
+            );
+          }, 1500);
+        } else if (dataSession == null) {
+          setTimeout(() => {
+            props.navigation.replace('SignIn', {tokenpn: data});
+          }, 1500);
+        }
+      });
     });
-
-  }, [])
-  
+  }, []);
 
   return (
     <View style={styles.main}>
+      <View style={styles.backgroundContainer}>
+        <ImageBackground source={mainBackground} style={styles.background} />
+      </View>
       <View style={styles.logoContainer}>
         <View style={styles.content}>
           <Image source={Logo} style={styles.logo} />
-          <Label title="Unklab Dormitory" textColor="#7BC9DE" textSize={25} tALight="center" mL={(width * 0.6) - 100} mT={-(height * 0.03)} />
+          <Label
+            title="Unklab Dormitory"
+            textColor="#7BC9DE"
+            textSize={width * 0.045}
+            tALight="center"
+            mL={width * 0.6 - 100}
+            mT={-(height * 0.03)}
+          />
         </View>
       </View>
     </View>
@@ -43,7 +59,6 @@ export default SplashScreen;
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -51,6 +66,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'absolute',
   },
   content: {
     alignItems: 'center',
@@ -58,8 +74,18 @@ const styles = StyleSheet.create({
     marginBottom: height * 0.03,
   },
   logo: {
-    width: width * 0.8,
+    width: width * 0.9,
     height: height * 0.2,
     resizeMode: 'contain',
+  },
+  backgroundContainer: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    opacity: 0.07,
   },
 });
